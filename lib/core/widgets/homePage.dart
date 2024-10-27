82247444e12fac242b461d92cdd49f60383ba41a
gilbert-ku <gilbert45ku@gmail.com>
2024-10-28 00:05:28 +0300

    create homepage and add bottom navigation bar using Gnav

---
+++ lib/core/widgets/homePage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 20,
+        ),
+        
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.green,
+          ),
+          
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 20,
+            ),
+            
+            child: GNav(
+              gap: 10,
+              backgroundColor: Colors.green,
+              color: Colors.white,
+              activeColor: Colors.pink,
+              tabBackgroundColor: Colors.white,
+              padding: EdgeInsets.all(20),
+              tabs: [
+                GButton(icon: Icons.home,
+                text: "Home"
+                ),
+                GButton(icon: Icons.info,
+                text: "About",
+                ),
+                GButton(icon: Icons.read_more,
+                text: "Blogs",
+                ),
+                GButton(icon: Icons.email,
+                text: "Contact Me",),
+                // GButton(icon: Icons.home),
+            
+              ]
+              ),
+          ),
+        ),
+      ),
+    );
+  }
+}
