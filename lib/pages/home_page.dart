7cada3f8d259a83ba21f566278169f2c4acde0e7
gilbert-ku <gilbert45ku@gmail.com>
2024-11-07 12:53:37 +0300

    add home screen appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  
+
   get child => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        // backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "g.ku",
+                style: GoogleFonts.pacifico(
+                  textStyle: TextStyle(
+                    fontSize: 40,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                
+              ),
+            ),
+          ],
+        ),
+      ),
       body: const Center(
         child: Text(
           "Hello Gilbert",
