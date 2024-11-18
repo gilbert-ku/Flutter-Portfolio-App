6b12d1f0a368b5c60c9fa12c3f7c764024d85d0e
gilbert-ku <gilbert45ku@gmail.com>
2024-11-18 16:47:03 +0300

    add skills widget

---
+++ lib/widgets/skills_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SkillsWidget extends StatelessWidget {
+  const SkillsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
