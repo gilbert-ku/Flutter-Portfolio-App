a3b30ce0b1745cfa8b87db82696e9c042c67792f
gilbert-ku <gilbert45ku@gmail.com>
2024-11-21 01:43:46 +0300

    create contact page form

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gku/widgets/contact/contact_form.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ContactPage extends StatelessWidget {
@@ -85,7 +86,7 @@ class ContactPage extends StatelessWidget {
               ),
             ],
           ),
-          
+          ContactForm()
         ],
       ),
     );
