b4de2621aaace423838347d082df093b48ede57c
gilbert-ku <gilbert45ku@gmail.com>
2024-12-13 15:37:15 +0300

    feat(contactSocialHandle): add social handle file and render this widget in the contact page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gku/widgets/contact/contact_form.dart';
+import 'package:gku/widgets/contact/contact_social_handle.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ContactPage extends StatelessWidget {
+
   const ContactPage({super.key});
 
   void _launchURL(Uri uri, bool inApp) async {
@@ -25,8 +27,6 @@ class ContactPage extends StatelessWidget {
     } catch (e) {
       // Log the error or handle it as needed
       debugPrint('Error launching URL: $e');
-      // Return a more descriptive error object or handle the error gracefully
-      // return 'Failed to launch URL. Please try again later.';
     }
   }
 
@@ -99,138 +99,8 @@ class ContactPage extends StatelessWidget {
                 ],
               ),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        // Left side: Contact and Email
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // call
-                            Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () => _launchURL(
-                                    Uri.parse(
-                                      "https://www.linkedin.com/in/gilbert-kutoto/",
-                                    ),
-                                    false,
-                                  ),
-                                  icon: FaIcon(
-                                    FontAwesomeIcons.phone,
-                                    size: 17,
-                                    color: Colors.pink,
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 8),
-                                  child: Text(
-                                    "+254 726 545 820",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 17,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            // SizedBox(height: 20), // Spacing between rows
-
-                            // email
-                            Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () => _launchURL(
-                                    Uri.parse(
-                                      "https://www.linkedin.com/in/gilbert-kutoto/",
-                                    ),
-                                    false,
-                                  ),
-                                  icon: FaIcon(
-                                    FontAwesomeIcons.message,
-                                    size: 17,
-                                    color: Colors.pink,
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 8),
-                                  child: Text(
-                                    "gilbert45ku@gmail.com",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontSize: 17,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-
-                        // Right side: WhatsApp and LinkedIn Icons
-                        // whatsapp icon
-                        Padding(
-                          padding: const EdgeInsets.only(right: 20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () => _launchURL(
-                                      Uri.parse(
-                                        "https://www.linkedin.com/in/gilbert-kutoto/",
-                                      ),
-                                      false,
-                                    ),
-                                    icon: FaIcon(
-                                      FontAwesomeIcons.whatsapp,
-                                      size: 20,
-                                      color: Colors.pink,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              // SizedBox(height: 10), // Spacing between rows
-
-                              // linkedin
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () => _launchURL(
-                                      Uri.parse(
-                                        "https://www.linkedin.com/in/gilbert-kutoto/",
-                                      ),
-                                      false,
-                                    ),
-                                    icon: FaIcon(
-                                      FontAwesomeIcons.linkedin,
-                                      size: 20,
-                                      color: Colors.pink,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              // handles contacts social handle
+              ContactSocialHandle(),
 
               SizedBox(
                 height: 10.0,
